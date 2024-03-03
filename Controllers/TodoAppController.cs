30a5f08347b3628153f5ee0649d33923a469c249
Aminetdi <63416927+Aminetdi@users.noreply.github.com>
2024-03-03 13:36:25 +0100

    Create Controller and method GetNotes

---
+++ Controllers/TodoAppController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ToDoAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoAppController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public TodoAppController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetNotes")]
+
+        public JsonResult GetNotes()
+        {
+            string query = "select * from Notes";
+            DataTable dataTable = new DataTable();
+            string sqlDatasources = _configuration.GetConnectionString("todoAppDBCon");
+            SqlDataReader reader;
+            using(SqlConnection connection = new SqlConnection(sqlDatasources))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    reader = command.ExecuteReader();
+                    dataTable.Load(reader);
+                }
+            }
+            return new JsonResult("");
+        }
+    }
+}
